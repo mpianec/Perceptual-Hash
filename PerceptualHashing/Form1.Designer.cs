5762fcc5bfafcd4a1826dc4a1e7a189eccb47c9f
mpianec@foi.hr <mpianec@foi.hr>
2018-12-23 18:50:33 +0100

    Add project files.

---
+++ PerceptualHashing/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace PerceptualHashing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.btnPrvaSlika = new System.Windows.Forms.Button();
+            this.btnDrugaSlika = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnaHash = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btndHash = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(61, 119);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(409, 247);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(564, 119);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(409, 247);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // btnPrvaSlika
+            // 
+            this.btnPrvaSlika.Location = new System.Drawing.Point(61, 74);
+            this.btnPrvaSlika.Name = "btnPrvaSlika";
+            this.btnPrvaSlika.Size = new System.Drawing.Size(136, 39);
+            this.btnPrvaSlika.TabIndex = 2;
+            this.btnPrvaSlika.Text = "Otvori prvu sliku";
+            this.btnPrvaSlika.UseVisualStyleBackColor = true;
+            this.btnPrvaSlika.Click += new System.EventHandler(this.btnPrvaSlika_Click);
+            // 
+            // btnDrugaSlika
+            // 
+            this.btnDrugaSlika.Location = new System.Drawing.Point(564, 74);
+            this.btnDrugaSlika.Name = "btnDrugaSlika";
+            this.btnDrugaSlika.Size = new System.Drawing.Size(132, 39);
+            this.btnDrugaSlika.TabIndex = 3;
+            this.btnDrugaSlika.Text = "Otvori drugu sliku";
+            this.btnDrugaSlika.UseVisualStyleBackColor = true;
+            this.btnDrugaSlika.Click += new System.EventHandler(this.btnDrugaSlika_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(182, 434);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // btnaHash
+            // 
+            this.btnaHash.Location = new System.Drawing.Point(383, 372);
+            this.btnaHash.Name = "btnaHash";
+            this.btnaHash.Size = new System.Drawing.Size(87, 29);
+            this.btnaHash.TabIndex = 5;
+            this.btnaHash.Text = "aHash";
+            this.btnaHash.UseVisualStyleBackColor = true;
+            this.btnaHash.Click += new System.EventHandler(this.btnaHash_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(182, 461);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(182, 487);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(182, 513);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(182, 539);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(58, 434);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(113, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Hash prve slike: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(58, 461);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Hash druge slike:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(58, 487);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(70, 17);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Isti bitovi: ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(58, 513);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(97, 17);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Različiti bitovi:";
+            // 
+            // btndHash
+            // 
+            this.btndHash.Location = new System.Drawing.Point(476, 372);
+            this.btndHash.Name = "btndHash";
+            this.btndHash.Size = new System.Drawing.Size(87, 29);
+            this.btndHash.TabIndex = 14;
+            this.btndHash.Text = "dHash";
+            this.btndHash.UseVisualStyleBackColor = true;
+            this.btndHash.Click += new System.EventHandler(this.btndHash_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1261, 594);
+            this.Controls.Add(this.btndHash);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnaHash);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDrugaSlika);
+            this.Controls.Add(this.btnPrvaSlika);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Perceptual Hashing";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button btnPrvaSlika;
+        private System.Windows.Forms.Button btnDrugaSlika;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnaHash;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btndHash;
+    }
+}
+
---
+++ PerceptualHashing/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PerceptualHashing
+{
+    public partial class Form1 : Form
+    {
+        string slika;
+        string hash1, hash2;
+        Bitmap img1, img2;
+        public Form1()
+        {
+            InitializeComponent();
+            Vidljivost(false);
+        }
+
+        private void btnPrvaSlika_Click(object sender, EventArgs e)
+        {           
+            img1 = new Bitmap(OtvoriSliku());
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.Image = img1;
+            
+            
+        }
+        public void Vidljivost(bool nekaj)
+        {
+            label1.Visible = nekaj;
+            label2.Visible = nekaj;
+            label3.Visible = nekaj;
+            label4.Visible = nekaj;
+            label5.Visible = nekaj;
+        }
+
+        public string OtvoriSliku() {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Title = "Images";
+            openFileDialog1.Filter = "All Images|*.jpg; *.bmp; *.png";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName.ToString() != "")
+            {
+                slika = openFileDialog1.FileName.ToString();
+            }
+            return slika;
+        }
+
+        private void btnDrugaSlika_Click(object sender, EventArgs e)
+        {
+            img2 = new Bitmap(OtvoriSliku());
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.Image = img2;
+        }
+
+        private void btnaHash_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                hash1 = aHash(img1);
+                hash2 = aHash(img2);
+                label1.Text = hash1;
+                label2.Text = hash2;
+                razlika(hash1, hash2);
+                Vidljivost(true);
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Treba prvo učitati slike!", "Upozorenje!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        public string aHash(Bitmap img)
+        {
+            string hash="";
+            Bitmap slika = new Bitmap(img, new Size(8, 8));
+            Bitmap grayscale;
+            int x, y,avg,a,r,g,b;
+            for (x = 0; x < slika.Width; x++)
+            {
+                for (y = 0; y < slika.Height; y++)
+                {
+                    Color pixColor = slika.GetPixel(x, y);
+                    a = pixColor.A;
+                    r = pixColor.R;
+                    g = pixColor.G;
+                    b = pixColor.B;
+                    avg = (r + g + b) / 3;
+                    slika.SetPixel(x, y,Color.FromArgb(a,avg,avg,avg));
+                }
+            }           
+            grayscale = slika;
+            int avgr=0, avgg=0, avgb=0;
+            for (x = 0; x < grayscale.Width; x++)
+            {
+                for (y = 0; y < grayscale.Height; y++)
+                {
+                    r = grayscale.GetPixel(x, y).R;
+                    g = grayscale.GetPixel(x, y).G;
+                    b = grayscale.GetPixel(x, y).B;
+                    avgr += r;
+                    avgg += g;
+                    avgb += b;
+                }
+            }
+            Color prosjek =Color.FromArgb(avgr/64,avgg/64,avgb/64);
+            
+            for (x = 0; x < grayscale.Width; x++)
+            {
+                for (y = 0; y < grayscale.Height; y++)
+                {
+                    if (grayscale.GetPixel(x, y).GetBrightness() < prosjek.GetBrightness())
+                        hash += "1";
+                    else
+                        hash += "0";
+                }
+            }
+            return hash; 
+        }
+
+        public string dHash (Bitmap img)
+        {
+            string hash="";
+            Bitmap slika = new Bitmap(img, new Size(9, 9));
+            Bitmap grayscale;
+            int x, y, avg, a, r, g, b;
+            for (x = 0; x < slika.Width; x++)
+            {
+                for (y = 0; y < slika.Height; y++)
+                {
+                    Color pixColor = slika.GetPixel(x, y);
+                    a = pixColor.A;
+                    r = pixColor.R;
+                    g = pixColor.G;
+                    b = pixColor.B;
+                    avg = (r + g + b) / 3;
+                    slika.SetPixel(x, y, Color.FromArgb(a, avg, avg, avg));
+                }
+            }
+            grayscale = slika;
+
+            string rHash = "";
+            for(int row = 0; row < 8; row++)
+            {
+                for(int column = 0; column < 8; column++)
+                {
+                    if (grayscale.GetPixel(column, row+1).R >= grayscale.GetPixel(column, row).R)
+                        rHash += "1";
+                    else
+                        rHash += "0";
+                }              
+            }
+            string cHash = "";
+            for (int column = 0; column < 8; column++)
+            {
+                for (int row = 0; row < 8; row++) {
+                    if (grayscale.GetPixel(column+1, row).R >= grayscale.GetPixel(column, row).R)
+                        cHash += "1";
+                    else
+                        cHash += "0";
+                }
+            }
+            hash = rHash+cHash;
+            return hash;
+        }
+
+        private void btndHash_Click(object sender, EventArgs e)
+        {
+            try
+            {               
+                hash1 = dHash(img1);
+                hash2 = dHash(img2);
+                MessageBox.Show(hash1.Length.ToString());
+                label1.Text = hash1;
+                label2.Text = hash2;
+                razlika(hash1, hash2);
+                Vidljivost(true);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Treba prvo učitati slike!", "Upozorenje!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        public void razlika(string neki, string neki2) {
+            var array1 = neki.ToCharArray();
+            var array2 = neki2.ToCharArray();
+            int isti=0, razliciti=0;
+            for(int x = 0; x < array1.Length; x++)
+            {
+                if (array1[x] == array2[x])
+                    isti += 1;
+                if(array1[x]!= array2[x])
+                    razliciti += 1;
+            }
+            double avg=0;
+            if (isti != 0 && razliciti != 0)
+                avg = (isti*100 / array1.Length);
+           // if (isti == 0 && razliciti!=0)
+            //    avg = 0;
+            if (isti!=0 && razliciti == 0)
+                avg = 100;
+            label5.Text = "Sličnost je: "+avg.ToString()+"%";
+            label3.Text = isti.ToString();
+            label4.Text = razliciti.ToString();
+        }
+      
+    }
+}
