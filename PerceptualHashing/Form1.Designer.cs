80e52e55c5a2ffa9fe7ef0ec7eaf527afe992678
mpianec@foi.hr <mpianec@foi.hr>
2019-01-05 22:19:01 +0100

    Dodan wHash.

--- PerceptualHashing/Form1.Designer.cs
+++ PerceptualHashing/Form1.Designer.cs
@@ -46,25 +46,26 @@
             this.label9 = new System.Windows.Forms.Label();
             this.btndHash = new System.Windows.Forms.Button();
             this.btnpHash = new System.Windows.Forms.Button();
+            this.btnwHash = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(46, 97);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(61, 119);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(307, 201);
+            this.pictureBox1.Size = new System.Drawing.Size(409, 247);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(423, 97);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Location = new System.Drawing.Point(564, 119);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(307, 201);
+            this.pictureBox2.Size = new System.Drawing.Size(409, 247);
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
@@ -78,10 +79,10 @@
             // 
             // btnPrvaSlika
             // 
-            this.btnPrvaSlika.Location = new System.Drawing.Point(46, 60);
-            this.btnPrvaSlika.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnPrvaSlika.Location = new System.Drawing.Point(61, 74);
+            this.btnPrvaSlika.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnPrvaSlika.Name = "btnPrvaSlika";
-            this.btnPrvaSlika.Size = new System.Drawing.Size(102, 32);
+            this.btnPrvaSlika.Size = new System.Drawing.Size(136, 39);
             this.btnPrvaSlika.TabIndex = 2;
             this.btnPrvaSlika.Text = "Otvori prvu sliku";
             this.btnPrvaSlika.UseVisualStyleBackColor = true;
@@ -89,10 +90,10 @@
             // 
             // btnDrugaSlika
             // 
-            this.btnDrugaSlika.Location = new System.Drawing.Point(423, 60);
-            this.btnDrugaSlika.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDrugaSlika.Location = new System.Drawing.Point(564, 74);
+            this.btnDrugaSlika.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnDrugaSlika.Name = "btnDrugaSlika";
-            this.btnDrugaSlika.Size = new System.Drawing.Size(99, 32);
+            this.btnDrugaSlika.Size = new System.Drawing.Size(132, 39);
             this.btnDrugaSlika.TabIndex = 3;
             this.btnDrugaSlika.Text = "Otvori drugu sliku";
             this.btnDrugaSlika.UseVisualStyleBackColor = true;
@@ -101,19 +102,18 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(136, 353);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(181, 434);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(46, 17);
             this.label1.TabIndex = 4;
             this.label1.Text = "label1";
             // 
             // btnaHash
             // 
-            this.btnaHash.Location = new System.Drawing.Point(287, 302);
-            this.btnaHash.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnaHash.Location = new System.Drawing.Point(383, 372);
+            this.btnaHash.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnaHash.Name = "btnaHash";
-            this.btnaHash.Size = new System.Drawing.Size(65, 24);
+            this.btnaHash.Size = new System.Drawing.Size(87, 30);
             this.btnaHash.TabIndex = 5;
             this.btnaHash.Text = "aHash";
             this.btnaHash.UseVisualStyleBackColor = true;
@@ -122,89 +122,81 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(136, 375);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(181, 462);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(46, 17);
             this.label2.TabIndex = 6;
             this.label2.Text = "label2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(136, 396);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(181, 487);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(46, 17);
             this.label3.TabIndex = 7;
             this.label3.Text = "label3";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(136, 417);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(181, 513);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(46, 17);
             this.label4.TabIndex = 8;
             this.label4.Text = "label4";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(136, 438);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(181, 539);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(46, 17);
             this.label5.TabIndex = 9;
             this.label5.Text = "label5";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(44, 353);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(59, 434);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.Size = new System.Drawing.Size(113, 17);
             this.label6.TabIndex = 10;
             this.label6.Text = "Hash prve slike: ";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(44, 375);
-            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Location = new System.Drawing.Point(59, 462);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(89, 13);
+            this.label7.Size = new System.Drawing.Size(118, 17);
             this.label7.TabIndex = 11;
             this.label7.Text = "Hash druge slike:";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(44, 396);
-            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Location = new System.Drawing.Point(59, 487);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(54, 13);
+            this.label8.Size = new System.Drawing.Size(70, 17);
             this.label8.TabIndex = 12;
             this.label8.Text = "Isti bitovi: ";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(44, 417);
-            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Location = new System.Drawing.Point(59, 513);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(74, 13);
+            this.label9.Size = new System.Drawing.Size(97, 17);
             this.label9.TabIndex = 13;
             this.label9.Text = "Različiti bitovi:";
             // 
             // btndHash
             // 
-            this.btndHash.Location = new System.Drawing.Point(357, 302);
-            this.btndHash.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btndHash.Location = new System.Drawing.Point(476, 372);
+            this.btndHash.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btndHash.Name = "btndHash";
-            this.btndHash.Size = new System.Drawing.Size(65, 24);
+            this.btndHash.Size = new System.Drawing.Size(87, 30);
             this.btndHash.TabIndex = 14;
             this.btndHash.Text = "dHash";
             this.btndHash.UseVisualStyleBackColor = true;
@@ -212,20 +204,31 @@
             // 
             // btnpHash
             // 
-            this.btnpHash.Location = new System.Drawing.Point(426, 302);
-            this.btnpHash.Margin = new System.Windows.Forms.Padding(2);
+            this.btnpHash.Location = new System.Drawing.Point(568, 372);
+            this.btnpHash.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnpHash.Name = "btnpHash";
-            this.btnpHash.Size = new System.Drawing.Size(65, 24);
+            this.btnpHash.Size = new System.Drawing.Size(87, 30);
             this.btnpHash.TabIndex = 15;
             this.btnpHash.Text = "pHash";
             this.btnpHash.UseVisualStyleBackColor = true;
             this.btnpHash.Click += new System.EventHandler(this.btnpHash_Click);
             // 
+            // btnwHash
+            // 
+            this.btnwHash.Location = new System.Drawing.Point(661, 372);
+            this.btnwHash.Name = "btnwHash";
+            this.btnwHash.Size = new System.Drawing.Size(87, 30);
+            this.btnwHash.TabIndex = 16;
+            this.btnwHash.Text = "wHash";
+            this.btnwHash.UseVisualStyleBackColor = true;
+            this.btnwHash.Click += new System.EventHandler(this.btnwHash_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(946, 483);
+            this.ClientSize = new System.Drawing.Size(1261, 594);
+            this.Controls.Add(this.btnwHash);
             this.Controls.Add(this.btnpHash);
             this.Controls.Add(this.btndHash);
             this.Controls.Add(this.label9);
@@ -242,7 +245,7 @@
             this.Controls.Add(this.btnPrvaSlika);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Perceptual Hashing";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -272,6 +275,7 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Button btndHash;
         private System.Windows.Forms.Button btnpHash;
+        private System.Windows.Forms.Button btnwHash;
     }
 }
 
--- PerceptualHashing/Form1.cs
+++ PerceptualHashing/Form1.cs
@@ -11,6 +11,7 @@ using System.IO;
 using Shipwreck.Phash;
 using Shipwreck.Phash.Bitmaps;
 using System.Collections;
+using System.Drawing.Imaging;
 
 namespace PerceptualHashing
 {
@@ -19,6 +20,10 @@ namespace PerceptualHashing
         string slika;
         string hash1, hash2;
         Bitmap img1, img2;
+        private const double w0 = 0.5;
+        private const double w1 = -0.5;
+        private const double s0 = 0.5;
+        private const double s1 = 0.5;
         public Form1()
         {
             InitializeComponent();
@@ -126,6 +131,79 @@ namespace PerceptualHashing
             return hash; 
         }
 
+        public string wHash(Bitmap img) {
+            string hash = "";
+            Bitmap slika = new Bitmap(img, new Size(32, 32));
+            Bitmap bmp; 
+            int x, y, avg, a, r, g, b;
+            for (x = 0; x < slika.Width; x++)
+            {
+                for (y = 0; y < slika.Height; y++)
+                {
+                    Color pixColor = slika.GetPixel(x, y);
+                    a = pixColor.A;
+                    r = pixColor.R;
+                    g = pixColor.G;
+                    b = pixColor.B;
+                    avg = (r + g + b) / 3;
+                    slika.SetPixel(x, y, Color.FromArgb(a, avg, avg, avg));
+                }
+            }
+            bmp = slika;
+            double[,] Red = new double[bmp.Width, bmp.Height];
+            double[,] Green = new double[bmp.Width, bmp.Height];
+            double[,] Blue = new double[bmp.Width, bmp.Height];
+            Color c;
+
+            for (int j = 0; j < bmp.Height; j++)
+            {
+                for (int i = 0; i < bmp.Width; i++)
+                {
+                    c = bmp.GetPixel(i, j);
+                    Red[i, j] = (double)Scale(0, 255, -1, 1, c.R);
+                    Green[i, j] = (double)Scale(0, 255, -1, 1, c.G);
+                    Blue[i, j] = (double)Scale(0, 255, -1, 1, c.B);
+                }
+            }
+            FWT(Red, 4);
+            FWT(Green, 4);
+            FWT(Blue, 4);
+            for (int j = 0; j < bmp.Height; j++)
+            {
+                for (int i = 0; i < bmp.Width; i++)
+                {
+                    bmp.SetPixel(i, j, Color.FromArgb((int)Scale(-1, 1, 0, 255, Red[i, j]), (int)Scale(-1, 1, 0, 255, Green[i, j]), (int)Scale(-1, 1, 0, 255, Blue[i, j])));
+                }
+            }
+            Bitmap promena = new Bitmap(bmp, new Size(8,8));
+       
+            int avgr = 0, avgg = 0, avgb = 0;
+            for (x = 0; x < promena.Width; x++)
+            {
+                for (y = 0; y < promena.Height; y++)
+                {
+                    r = promena.GetPixel(x, y).R;
+                    g = promena.GetPixel(x, y).G;
+                    b = promena.GetPixel(x, y).B;
+                    avgr += r;
+                    avgg += g;
+                    avgb += b;
+                }
+            }
+            Color prosjek = Color.FromArgb(avgr / 64, avgg / 64, avgb / 64);
+            for (x = 0; x < promena.Width; x++)
+            {
+                for (y = 0; y < promena.Height; y++)
+                {
+                    if (promena.GetPixel(x, y).GetBrightness() < prosjek.GetBrightness())
+                        hash += "1";
+                    else
+                        hash += "0";
+                }
+            }
+           // pictureBox2.Image = promena;
+            return hash;
+        }
         public string dHash (Bitmap img)
         {
             string hash="";
@@ -207,7 +285,6 @@ namespace PerceptualHashing
             {               
                 hash1 = dHash(img1);
                 hash2 = dHash(img2);
-                MessageBox.Show(hash1.Length.ToString());
                 label1.Text = hash1;
                 label2.Text = hash2;
                 razlika(hash1, hash2);
@@ -250,6 +327,87 @@ namespace PerceptualHashing
                 output += bitArray.Get(i) ? "1" : "0";
             }
             return output;
+        }
+
+        public void FWT(double[] data)
+        {
+            double[] temp = new double[data.Length];
+
+            int h = data.Length >> 1;
+            for (int i = 0; i < h; i++)
+            {
+                int k = (i << 1);
+                temp[i] = data[k] * s0 + data[k + 1] * s1;
+                temp[i + h] = data[k] * w0 + data[k + 1] * w1;
+            }
+
+            for (int i = 0; i < data.Length; i++)
+                data[i] = temp[i];
+        }
+
+        private void btnwHash_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                hash1 = wHash(img1);
+                hash2 = wHash(img2);
+                label1.Text = hash1;
+                label2.Text = hash2;
+                razlika(hash1, hash2);
+                Vidljivost(true);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Treba prvo učitati slike!", "Upozorenje!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        public void FWT(double[,] data, int iterations)
+        {
+            int rows = data.GetLength(0);
+            int cols = data.GetLength(1);
+
+            double[] row = new double[cols];
+            double[] col = new double[rows];
+
+            for (int k = 0; k < iterations; k++)
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < row.Length; j++)
+                        row[j] = data[i, j];
+
+                    FWT(row);
+
+                    for (int j = 0; j < row.Length; j++)
+                        data[i, j] = row[j];
+                }
+
+                for (int j = 0; j < cols; j++)
+                {
+                    for (int i = 0; i < col.Length; i++)
+                        col[i] = data[i, j];
+
+                    FWT(col);
+
+                    for (int i = 0; i < col.Length; i++)
+                        data[i, j] = col[i];
+                }
+            }
+        }
+        public double Scale(double fromMin, double fromMax, double toMin, double toMax, double x)
+        {
+            if (fromMax - fromMin == 0) return 0;
+            double value = (toMax - toMin) * (x - fromMin) / (fromMax - fromMin) + toMin;
+            if (value > toMax)
+            {
+                value = toMax;
+            }
+            if (value < toMin)
+            {
+                value = toMin;
+            }
+            return value;
         }
     }
 }
